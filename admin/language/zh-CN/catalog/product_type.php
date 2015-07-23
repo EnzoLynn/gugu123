9fe63dbac4386e6bb06f461b29c657820bc169ad
zhouhui <95459910@qq.com>
2015-07-23 14:16:23 +0800

    zhouhui

---
+++ admin/language/zh-CN/catalog/product_type.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hui.zhou
+ * Date: 2015/7/2
+ * Time: 10:35
+ */
+// Heading
+$_['heading_title']     = '商品类型';
+
+// Text
+$_['text_success']      = '成功: 已经修改商品类型！';
+$_['text_list']         = '属性分组列表';
+$_['text_add']          = '添加商品类型';
+$_['text_edit']         = '编辑商品类型';
+
+// Column
+$_['column_id']       = 'ID';
+$_['column_name']       = '商品类型';
+$_['column_sort_order'] = '排序';
+$_['column_action']     = '操作';
+$_['column_attribute_num']     = '属性数目';
+
+// Entry
+$_['entry_type_name']        = '商品类型';
+$_['entry_filter_group_ids']        = '过滤项';
+$_['entry_attribute_ids']        = '属性项';
+
+
+// Error
+$_['error_permission']  = '警告: 无权限修改商品类型！';
+$_['error_name']        = '商品类型名称必须介于3-64字符之间！';
+$_['error_attribute']   = '警告: 不能删除此属性分组，该属性分组已经关联到 %s 属性中！';
+$_['error_product']     = '警告: 不能删除此属性分组，该属性分组已经关联到 %s 商品中！';
--- admin/model/catalog/product_type.php
+++ admin/model/catalog/product_type.php
@@ -5,50 +5,50 @@
  * Date: 2015/7/2
  * Time: 10:24
  */
-class ModelCatalogAttributeGroupCustomize extends Model
+class ModelCatalogProductType extends Model
 {
-    public function addAttributeGroupCustomize($data)
+    public function addProductType($data)
     {
-        $this->event->trigger('pre.admin.attribute_group_customize.add', $data);
+        $this->event->trigger('pre.admin.product_type.add', $data);
 
-        $this->db->query("INSERT INTO " . DB_PREFIX . "attribute_group_customize SET customize_name = '" . $this->db->escape($data['customize_name']) . "', filter_group_ids = '" .  $this->db->escape($data['filter_group_ids']) . "', attribute_ids = '" .  $this->db->escape($data['attribute_ids']) . "'");
+        $this->db->query("INSERT INTO " . DB_PREFIX . "product_type SET type_name = '" . $this->db->escape($data['type_name']) . "', filter_group_ids = '" .  $this->db->escape($data['filter_group_ids']) . "', attribute_ids = '" .  $this->db->escape($data['attribute_ids']) . "'");
 
         $customize_id = $this->db->getLastId();
 
-        $this->event->trigger('post.admin.attribute_group_customize.add', $customize_id);
+        $this->event->trigger('post.admin.product_type.add', $customize_id);
 
         return $customize_id;
     }
 
-    public function editAttributeGroupCustomize($customize_id, $data)
+    public function editProductType($type_id, $data)
     {
-        $this->event->trigger('pre.admin.attribute_group_customize.edit', $data);
+        $this->event->trigger('pre.admin.product_type.edit', $data);
 
-        $this->db->query("UPDATE " . DB_PREFIX . "attribute_group_customize SET customize_name = '" . $this->db->escape($data['customize_name']) . "', filter_group_ids = '" .  $this->db->escape($data['filter_group_ids']) . "', attribute_ids = '" .  $this->db->escape($data['attribute_ids']) . "' WHERE customize_id = '" . (int)$customize_id . "'");
+        $this->db->query("UPDATE " . DB_PREFIX . "product_type SET type_name = '" . $this->db->escape($data['type_name']) . "', filter_group_ids = '" .  $this->db->escape($data['filter_group_ids']) . "', attribute_ids = '" .  $this->db->escape($data['attribute_ids']) . "' WHERE type_id = '" . (int)$type_id . "'");
 
-        $this->event->trigger('post.admin.attribute_group_customize.edit', $customize_id);
+        $this->event->trigger('post.admin.product_type.edit', $type_id);
     }
 
-    public function deleteAttributeGroupCustomize($customize_id)
+    public function deleteProductType($type_id)
     {
-        $this->event->trigger('pre.admin.attribute.delete', $customize_id);
+        $this->event->trigger('pre.admin.product_type.delete', $type_id);
 
-        $this->db->query("DELETE FROM " . DB_PREFIX . "attribute_group_customize WHERE attribute_id = '" . (int)$customize_id . "'");
+        $this->db->query("DELETE FROM " . DB_PREFIX . "product_type WHERE attribute_id = '" . (int)$type_id . "'");
 
-        $this->event->trigger('post.admin.attribute.delete', $customize_id);
+        $this->event->trigger('post.admin.product_type.delete', $type_id);
     }
 
-    public function getAttributeGroupCustomize($customize_id)
+    public function getProductType($type_id)
     {
 
-        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "attribute_group_customize WHERE customize_id = '" . (int)$customize_id . "'");
+        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "product_type WHERE type_id = '" . (int)$type_id . "'");
 
         return $query->row;
     }
 
-    public function getAttributeGroupsCustomizes()
+    public function getProductTypes()
     {
-        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "attribute_group_customize ORDER BY customize_id");
+        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "product_type ORDER BY type_id");
 
         return $query->rows;
     }
