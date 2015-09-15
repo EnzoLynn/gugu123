c254eeed49181f7552bc873a2307bea6f4c2e948
chenheling <6710582@qq.com>
2015-09-15 11:02:18 +0800

    'express'

---
+++ routes/api.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var apiConfig = require('../routes/apiConfig.js');
+//var path = require('path'); 
+
+/* get 咨询 */
+router.get('/getSupportMore', function(req, res, next) { 
+	res.status(200).sendfile(apiConfig.apiConfig.getSupportMore);
+	//res.sendfile(path.join(__dirname,apiConfig.apiConfig.getSupportMore)); 
+});
+// get 评价
+router.get('/getEvaMore', function(req, res, next) { 
+	res.status(200).sendfile(apiConfig.apiConfig.getEvaMore); 
+});
+
+// post one 加入购物车
+router.post('/postOneJoinCart', function(req, res, next) { 
+	res.status(200).send({suc:true}); 
+});
+
+// post  提交咨询
+router.post('/postSupport', function(req, res, next) { 
+	res.status(200).send({suc:true}); 
+});
+
+//post detail 加入购物车
+router.post('/postDetailJoinCart', function(req, res, next) { 
+	res.status(200).send({suc:true}); 
+});
+
+
+//post logfloor 删除购物车项目
+router.post('/postDelCartItem', function(req, res, next) { 
+	res.status(200).sendfile(apiConfig.apiConfig.postDelCartItem); 
+});
+
+module.exports = router;
---
+++ routes/apiConfig.js
@@ -0,0 +1,8 @@
+var apiConfig = {
+	getSupportMore:'./data/supportData.json',
+	getEvaMore:'./data/evaluationData.json',
+	postDelCartItem:'./data/cart-proData.json'
+};
+
+
+exports.apiConfig = apiConfig;
